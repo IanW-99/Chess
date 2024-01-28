1d96743cf2db2a49e17930e71bcc567b060cbf0c
Ian Washburn <iwashburn99@gmail.com>
2024-01-28 18:01:05 -0500

    created board state structure

--- Board.py
+++ Board.py
@@ -9,6 +9,7 @@ class Board:
         self.square_width = width // 8
         self.square_height = height // 8
         self.squares = self.create_squares()
+        self.board_state = []
 
     def create_squares(self):
         squares = []
@@ -17,6 +18,18 @@ class Board:
                 square = Square(x, y, self.square_width, self.square_height)
                 squares.append(square)
         return squares
+
+    def place_initial_pieces(self):
+        self.board_state = [
+            ['bRook', 'bKnight', 'bBishop', 'bQueen', 'bKing', 'bBishop', 'bKnight', 'bRook'],
+            ['bPawn', 'bPawn', 'bPawn', 'bPawn', 'bPawn', 'bPawn', 'bPawn', 'bPawn'],
+            ['', '', '', '', '', '', '', ''],
+            ['', '', '', '', '', '', '', ''],
+            ['', '', '', '', '', '', '', ''],
+            ['', '', '', '', '', '', '', ''],
+            ['wPawn', 'wPawn', 'wPawn', 'wPawn', 'wPawn', 'wPawn', 'wPawn', 'wPawn'],
+            ['wRook', 'wKnight', 'wBishop', 'wQueen', 'wKing', 'wBishop', 'wKnight', 'wRook'],
+        ]
 
     def draw_board(self, screen):
         for square in self.squares:
