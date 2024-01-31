dec22b4a4c5737d09efa76f22a8d2d3b088cdd5f
Ian Washburn <iwashburn99@gmail.com>
2024-01-30 21:49:11 -0500

    board click reg

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ clock = pygame.time.Clock()
 board_size = 640
 board_surface = pygame.Surface((board_size, board_size))
 
+board_x = window_size[0] // 2 - board_size // 2
+board_y = window_size[1] // 2 - board_size // 2
 
 def draw():
     # draw board surface
@@ -19,15 +21,29 @@ def draw():
     board.draw_board(board_surface)
 
     # draw board surface onto screen
-    screen.blit(board_surface, ((window_size[0] // 2 - board_size // 2), (window_size[1] // 2 - board_size // 2)))
+    screen.blit(board_surface, (board_x, board_y))
+
+
+def is_on_board(x, y):
+    if (board_x <= x <= board_x+board_size) and (board_y <= y <= board_y+board_size):
+        return True
+    else:
+        return False
 
 
 while True:
     # Process player inputs.
+    mouse = pygame.mouse.get_pos()
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             raise SystemExit
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if is_on_board(mouse[0], mouse[1]):
+                print('on board')
+            else:
+                print('not on board')
 
     draw()
 
